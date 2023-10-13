47a25f868b0ab97a0a386a6d0c1f846e6617a6a9
Lucas Carvalho <lucas.cdsilva@gmail.com>
2023-10-12 20:12:48 -0400

    ajustes na logo e favicon

--- src/header/header.js
+++ src/header/header.js
@@ -9,7 +9,7 @@ export default function Header() {
         <div class="flex lg:flex-1">
           <a href="#" class="-m-1.5 p-1.5">
             <span class="sr-only">Sistema Manancial</span>
-            <img class="h-10 w-auto" src="${logo}" alt="">
+            <img class="h-12 w-auto" src="${logo}" alt="">
           </a>
         </div>
         <div class="flex lg:hidden">
